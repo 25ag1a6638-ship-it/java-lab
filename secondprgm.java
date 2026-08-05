6672bb653b3fdb5c1b20f16c2798f2f56bf6a4e5
Rohan Goud <25ag1a6638@gmail.com>
2026-08-05 15:01:26 +0530

    Add InstanceVariable class with show and display methods

---
+++ secondprgm.java
@@ -0,0 +1,17 @@
+class InstanceVariable {
+    int x = 100;
+
+    public void show() {
+        System.out.println("Inside show method, x = " + x);
+        x = x + 100;
+    }
+    public void display() {
+        System.out.println("Inside display method, x = " + x);
+    }
+
+    public static void main(String[] args) {
+        InstanceVariable obj = new InstanceVariable(); // Fixed here
+        obj.show();
+        obj.display();
+    }
+}
