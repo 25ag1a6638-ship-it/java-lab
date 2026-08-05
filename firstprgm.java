a53cee58d164cdef070b11505e389c9809ff59fb
Rohan Goud <25ag1a6638@gmail.com>
2026-08-05 15:48:12 +0530

    Update firstprgm.java

--- firstprgm.java
+++ firstprgm.java
@@ -1,1 +1,19 @@
-
+class Animal {
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+public class DogMethod {
+    public static void main(String[] args) {
+        Animal mypet = new Dog();
+        Animal mypet1 = new Animal();
+        mypet.sound();
+        mypet1.sound();
+    }
+}
